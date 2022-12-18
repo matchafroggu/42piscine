787c9cba1ae406d600e69128b2192ab5d2617dae
Maple Ang <mang@F2N046.42adel.org.au>
2022-12-18 14:21:27 +1030

    ex01

--- c08/ex01/main.c
+++ c08/ex01/main.c
@@ -1,26 +1,34 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_boolean.h                                       :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mang <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/17 16:44:37 by mang              #+#    #+#             */
-/*   Updated: 2022/12/18 14:08:00 by mang             ###   ########.fr       */
+/*   Created: 2022/12/18 13:47:30 by mang              #+#    #+#             */
+/*   Updated: 2022/12/18 14:04:17 by mang             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_BOOLEAN_H
-# define FT_BOOLEAN_H
+#include "ft_boolean.h"
 
-# include <unistd.h>
-# define EVEN(n) n % 2 == 0
-# define TRUE 1
-# define FALSE 0
-# define EVEN_MSG "I have an even number of arguments.\n"
-# define ODD_MSG "I have an odd number of arguments.\n"
-# define SUCCESS 0
+void	ft_putstr(char *str)
+{
+	while (*str)
+		write(1, str++, 1);
+}
 
-typedef int	t_bool;
+t_bool	ft_is_even(int nbr)
+{
+	return ((EVEN(nbr)) ? TRUE : FALSE);
+}
 
-#endif
+int	main(int argc, char **argv)
+{
+	(void)argv;
+	if (ft_is_even(argc - 1) == TRUE)
+		ft_putstr(EVEN_MSG);
+	else
+		ft_putstr(ODD_MSG);
+	return (SUCCESS);
+}
